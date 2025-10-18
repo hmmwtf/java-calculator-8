48c5f27fcd2b241aeb5b6d804ee13ef791bc4771
hmmwtf <94499659+hmmwtf@users.noreply.github.com>
2025-10-19 00:16:59 +0900

    feat: 사용자 입력 기능 및 예외 처리 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,19 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String input = br.readLine();
+            System.out.println("input: " + input);
+
+        } catch (IOException e) {
+            throw new IllegalArgumentException("입력 처리 중 오류가 발생했습니다.");
+        }
     }
 }
