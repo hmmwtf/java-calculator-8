b7d23731ee1002d217e8255133a6a3548d802043
hmmwtf <94499659+hmmwtf@users.noreply.github.com>
2025-10-19 11:24:37 +0900

    feat: 기본 및 커스텀 구분자 파싱 기능 일부 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,12 +5,23 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
 
         if (input.isEmpty()) {
-            System.out.println("0");
+            System.out.println("결과 : 0");
             return;
         }
+
+        String[] numbers;
+        String delimiter = ",|:";
+
+        if (input.startsWith("//")) {
+            int lineIndex = input.indexOf("\n");
+            delimiter = input.substring(2, lineIndex);
+            input = input.substring(lineIndex + 1);
+        }
+
 
     }
 }
